d1583d0bf03a7c1d527cdb7dcc0b418fafad39d6
Soroush mohammadi <sorooshm78@gmail.com>
2024-11-30 20:37:47 +0330

    Update uniq.cpp

--- src/uniq.cpp
+++ src/uniq.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
-
+template<typename T>
 class UniquePtr {
+public:
+    UniquePtr(T *pointer) {
+        this->pointer = pointer;
+    }
+
+    T operator*() {
+        return *pointer;
+    }
+private:
+    T *pointer;
 };
 
 
 int main() {
-    cout << "Hello World!" << endl;
+    unique_ptr<int> p(new int);
+    *p = 1;
+    cout << *p << endl;
+
+    UniquePtr<int> ptr(new int);
+    cout << *ptr << endl;
 }
